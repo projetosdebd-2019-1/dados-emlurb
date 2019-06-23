c77b3b3f2728321077b89df73bbb8ddd8e6ad55a
Italo Sousa <italo.andrade@gmail.com>
2019-06-23 13:35:49 -0300

    add heatmap

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,23 @@ const dest = './src/app/assets';
 
 const styles = [
   './node_modules/bootstrap/dist/css/bootstrap.min.css',
+  './node_modules/leaflet/dist/leaflet.css',
+  './src/app/style.css'
 ];
 
 const scripts = [
   './node_modules/jquery/dist/jquery.min.js',
-  './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+  './node_modules/axios/dist/axios.min.js',
+  './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+  './node_modules/leaflet/dist/leaflet.js',
+  './src/app/libs/leaflet-heat.js',
+  './src/app/src/app.js',
 ];
 
 gulp.task('build-styles', () => {
   return gulp
     .src(styles)
+    .pipe(concat('bundle.css'))
     .pipe(gulp.dest(`${dest}/css`));
 });
 
---
+++ src/app/src/app.js
@@ -0,0 +1,52 @@
+(function App(Leaflet, axios, $) {
+
+  const map = Leaflet.map('map').setView([-8.056925, -34.883004], 13); // Recife view
+  let addressPoints = [];
+  let heatLayer;
+
+  const setupMap = () => {
+    Leaflet.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    heatLayer = Leaflet.heatLayer(addressPoints).addTo(map);
+  };
+
+  const setMapPoints = (latlngs) => {
+    heatLayer.setLatLngs(latlngs).addTo(map);
+  };
+
+  const setAddressPoints = (addresses) => {
+    addressPoints = addresses.map((address) => [address.lat, address.situacao]);
+  };
+
+  const setMapDescription = (description) => {
+    $('#heatmap-desc').text(description);
+  }
+
+  const loadMapData = () => {
+    axios.get('/heat-map')
+      .then((response) => {
+        const { data } = response;
+        console.log(data);
+        setMapDescription(data.definition);
+        setAddressPoints(data.data);
+        setMapPoints(addressPoints);
+      }).catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // run
+  const run = async () => {
+
+    setupMap();
+    loadMapData();
+
+  };
+
+  run();
+})(L, axios, $);
+
+
+
+
