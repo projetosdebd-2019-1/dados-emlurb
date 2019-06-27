bc30aab21879dd47e9f9cad53e8fbaff8c6f28a2
Bruno <brunoalvescall@gmail.com>
2019-06-27 00:37:09 -0300

    rotas

--- src/controllers/heat-map-controller.js
+++ src/controllers/heat-map-controller.js
@@ -1,6 +1,29 @@
 const database = require('../config/database');
 
 module.exports = {
+  getMenorAtuacao1(ano) {
+    console.log('epa')
+    return new Promise((resolve, reject) => {
+      database.query(`
+      select lat, lng
+      from endereco, chamado
+      where endereco.codigo = chamado.codigoEndereco
+      and situacao = 'PENDENTE'
+      and EXTRACT(YEAR from dataDemanda) = ${ano}
+      and endereco.bairro
+      in (select * from (select bairro
+      from endereco, chamado
+      where chamado.situacao = 'PENDENTE'
+      and EXTRACT(YEAR from dataDemanda) = ${ano}
+      and chamado.codigoEndereco = endereco.codigo
+      group by bairro
+      order by count(logradouro) DESC LIMIT 3) temp_tab);
+      `, (err, rows, fields) => {
+        resolve(rows);
+      });
+    });
+  },
+
   getMenorAtuacao() {
     return new Promise((resolve, reject) => {
       database.query(`
@@ -55,6 +78,82 @@ module.exports = {
         resolve(rows);
       });
     });
+  },
+
+  getSitucoes(){
+    return new Promise((resolve, reject) => {
+      database.query(`SELECT DISTINCT(situacao) FROM chamado;`, (err, rows, fields) => {
+        resolve(rows);
+      });
+    });
+  },
+
+  getMaisRecorrentes1(bairro, ano) {
+    return new Promise((resolve, reject) => {
+      database.query(`
+      select lat, lng, descricao from endereco, servico, chamado
+      where chamado.codigoServico = servico.id
+      and endereco.codigo = chamado.codigoEndereco
+      and endereco.bairro = '${bairro}'
+      and EXTRACT(YEAR from dataDemanda) = ${ano}
+      and servico.descricao in (select * from (select servico.descricao from chamado, endereco, servico
+      where chamado.codigoEndereco = endereco.codigo
+      and chamado.codigoServico = servico.id
+      and EXTRACT(YEAR from dataDemanda) = ${ano}
+      and bairro = '${bairro}'
+      group by servico.descricao
+      order by count(servico.descricao) DESC LIMIT 3) temp_tab);
+      `, (err, rows, fields) => {
+        resolve(rows);
+      });
+    });
+  },
+
+  getTodosChamados() {
+    return new Promise((resolve, reject) => {
+      database.query(`
+      select lat, lng from chamado, endereco
+      where chamado.codigoEndereco = endereco.codigo;
+      `, (err, rows, fields) => {
+        resolve(rows);
+      });
+    });
+  },
+
+  getTodosChamados1(ano){
+    return new Promise((resolve, reject) => {
+      database.query(`
+      select lat, lng from chamado, endereco
+      where chamado.codigoEndereco = endereco.codigo
+      and EXTRACT(YEAR from dataDemanda) = ${ano};
+      `, (err, rows, fields) => {
+        resolve(rows);
+      });
+    })
+  },
+
+  getTodosChamados2(bairro){
+    return new Promise((resolve, reject) => {
+      database.query(`
+      select lat, lng from chamado, endereco
+      where chamado.codigoEndereco = endereco.codigo
+      and bairro = '${bairro}';
+      `, (err, rows, fields) => {
+        resolve(rows);
+      });
+    })
+  },
+
+  getTodosChamados3(situacao){
+    return new Promise((resolve, reject) => {
+      database.query(`
+      select lat, lng from chamado, endereco
+      where chamado.codigoEndereco = endereco.codigo
+      and situacao = '${situacao}';
+      `, (err, rows, fields) => {
+        resolve(rows);
+      });
+    })
   },
 
 
--- src/routes/heat-map-router.js
+++ src/routes/heat-map-router.js
@@ -2,22 +2,55 @@ const { Router } = require('express');
 const controller = require('../controllers/heat-map-controller');
 
 const router = Router();
-
+//MENOR ATUAÇÃO STARTA NORMAL, E TEM FILTRO DE ANO
 router.get('/MenorAtuacao', async (req, res) => {
-  const data = await controller.getMenorAtuacao();
+  const ano = req.query.ano;
+  console.log(ano)
+  let data;
+  if (ano != undefined){
+    data = await controller.getMenorAtuacao1(ano);
+  }
+  else{
+    data = await controller.getMenorAtuacao();
+  }
   res.json({
-    definition: 'Bairros com menos atuação da emlurb; Chamados com situação pendente',
+    definition: 'Bairros com menos atuação da emlurb; Chamados com situação pendente em:' + `${ano}`,
     data,
   });
 });
 
+//SERVICOS RECORRENTES, OBRIGATÓRIO FILTRO DE BAIRRO e POSSIBILIDADE DE FILTRO DE ANO
 router.get('/ServicosRecorrentes', async (req, res) => {
   const bairro = req.query.bairro;
-  const data = await controller.getMaisRecorrentes(bairro);
+  const ano = req.query.ano;
+  let data;
+  if(bairro != undefined && ano == undefined ){
+    data = await controller.getMaisRecorrentes(bairro);
+  }
+  else if(bairro != undefined && ano != undefined ){
+    data = await controller.getMaisRecorrentes1(bairro, ano);
+  }
   res.json({
-    definition: 'Servicos mais recorrentes em: ',
+    definition: `Servicos mais recorrentes em: ${bairro} no ano de : ${ano}`,
     data,
   });
 });
+
+router.get('/TodosChamados', async (req, res) => {
+  const data = await controller.getTodosChamados();
+  res.json({
+    definition: "Todos os chamados",
+    data,
+  })
+})
+
+//situacoes para o filtro
+router.get('/Situacao', async (req, res) => {
+  const data =  await controller.getSitucoes();
+  res.json({
+    data,
+  });
+});
+
 
 module.exports = router;
